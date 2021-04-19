911208c4cab3ea220fd9a11a0122830ab3f670c1
Bin Wang <wkk@users.noreply.github.com>
2021-04-19 10:31:20 -0400

    Update comment

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # #!/usr/bin/env bash
 set -e
 
-# us-east-2a to us-east-2c
+# us-east-2a to us-east-2c, 1 server
 terraform apply -var load-generator-region=us-east-2 \
                 -var cluster-region=us-east-2 \
                 -var cluster-availability-zone=c
@@ -14,7 +14,7 @@ terraform destroy -var load-generator-region=us-east-2 \
                 -var cluster-availability-zone=c
                 -var worker-count=1 -auto-approve
 
-# us-east-2 to ca-central-1
+# us-east-2a to ca-central-1a, 10 servers
 terraform apply -var load-generator-region=eu-east-2 \
                 -var cluster-region=ca-central-1 \
                 -var worker-count=10 -auto-approve
