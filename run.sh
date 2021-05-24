8f35361352da6cc00c19431ba9914b33a13bc8b8
Bin Wang <wkk@users.noreply.github.com>
2021-05-24 17:11:20 -0400

    Fix error in script

--- run.sh
+++ run.sh
@@ -4,23 +4,23 @@ set -e
 # us-east-2a to us-east-2c, 1 server
 terraform apply -var load-generator-region=us-east-2 \
                 -var cluster-region=us-east-2 \
-                -var cluster-availability-zone=c
+                -var cluster-availability-zone=c \
                 -var worker-count=1 -auto-approve
 
 (cd ansible && ansible-playbook ping.yml && ansible-playbook setup.yml && ansible-playbook experiment.yml)
 
 terraform destroy -var load-generator-region=us-east-2 \
                 -var cluster-region=us-east-2 \
-                -var cluster-availability-zone=c
+                -var cluster-availability-zone=c \
                 -var worker-count=1 -auto-approve
 
 # us-east-2a to ca-central-1a, 10 servers
-terraform apply -var load-generator-region=eu-east-2 \
+terraform apply -var load-generator-region=us-east-2 \
                 -var cluster-region=ca-central-1 \
                 -var worker-count=10 -auto-approve
 
 (cd ansible && ansible-playbook ping.yml && ansible-playbook setup.yml && ansible-playbook experiment.yml)
 
-terraform apply -var load-generator-region=eu-east-2 \
+terraform destroy -var load-generator-region=us-east-2 \
                 -var cluster-region=ca-central-1 \
                 -var worker-count=10 -auto-approve
